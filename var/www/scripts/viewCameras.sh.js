8c2f3329f1025faa826fc5e0261a63d309d85090
opalenzuela <opalenzuela@gmail.com>
2014-10-01 16:47:01 +0200

    using shorthand property

--- var/www/scripts/viewCameras.sh.js
+++ var/www/scripts/viewCameras.sh.js
@@ -4,6 +4,6 @@ setInterval(refreshCameras,5000);
 function refreshCameras() {
 	$("li.camera").each(function(index){
 		var filename = "/data/" + $(this).attr("id")  + ".jpeg";
-		$(this).find("a").css("background-image","url(" + filename + ");")
+		$(this).find("a").css("background","url(" + filename + ") no-repeat center center");
 	});
 }
