fb462a527d6f019b180faebff85c2d77e0de5780
opalenzuela <opalenzuela@gmail.com>
2014-10-01 16:43:51 +0200

    removing quote

--- var/www/scripts/viewCameras.sh.js
+++ var/www/scripts/viewCameras.sh.js
@@ -4,6 +4,6 @@ setInterval(refreshCameras,5000);
 function refreshCameras() {
 	$("li.camera").each(function(index){
 		var filename = "/data/" + $(this).attr("id")  + ".jpeg";
-		$(this).find("a").css("background-image","url('" + filename + "');")
+		$(this).find("a").css("background-image","url(" + filename + ");")
 	});
 }
