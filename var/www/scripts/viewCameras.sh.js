7ed6d6e1f1cdc2dc27c59d3742febfd722993fe4
opalenzuela <opalenzuela@gmail.com>
2014-10-01 17:11:05 +0200

    interval

--- var/www/scripts/viewCameras.sh.js
+++ var/www/scripts/viewCameras.sh.js
@@ -1,8 +1,8 @@
 
-setInterval(refreshCameras,5000);
+setInterval(refreshCameras,1000);
 
 function refreshCameras() {
-	$("li.camera").each(function(index){
+	$("li.camera, li.zoomedcamera").each(function(index){
 		var filename = "/data/" + $(this).attr("id")  + ".jpeg";
 		$(this).find("a").css("background","url(" + filename + ") no-repeat center center");
 	});
