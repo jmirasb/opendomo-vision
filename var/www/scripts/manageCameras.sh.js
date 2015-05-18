1592717f3ecd59764db5176b600758232ae4828d
opalenzuela <opalenzuela@gmail.com>
2015-05-18 13:30:08 +0200

    Implementing loadAsync

--- var/www/scripts/manageCameras.sh.js
+++ var/www/scripts/manageCameras.sh.js
@@ -5,13 +5,13 @@ $("#submit-saveSettings").on("click",function(e){
 		$(this).addClass("loading");
 		if ($(this).hasClass("selected")){
 			var filter = $(this).id;
-			var camera = $("#code");
+			var camera = $("#code").val();
 			loadAsync("./addFilterToCamera.sh?camera="+camera+"&filter="+filter,function(){
 				console.log("Saved "+filter);
 			});
 		} else {
 			var filter = $(this).id;
-			var camera = $("#code");
+			var camera = $("#code").val();
 			loadAsync("./removeFilterToCamera.sh?camera="+camera+"&filter="+filter,function(){
 				console.log("Saved "+filter);
 			});			
@@ -19,3 +19,12 @@ $("#submit-saveSettings").on("click",function(e){
 		$(this).removeClass("loading");		
 	});
 });
+
+if (typeof loadAsync == "undefined") {
+	function loadAsync(filePath, callback){
+		jQuery.ajax({
+			type : "GET",
+			url : filePath
+		}).success(callback);		
+	}
+}
